0bb64137f906303cf4e7fc693bf09df90310b36e
rajnishkr2308 <raj123rajnish2308@gmail.com>
2023-05-02 18:26:51 +0530

    Added:Find the Integer Maximum

--- src/com/bridgelabz/TestMaximum.java
+++ src/com/bridgelabz/TestMaximum.java
@@ -1,7 +1,28 @@
 package com.bridgelabz;
 
 public class TestMaximum {
+    /*
+        Three Integers to find the maximum
+     */
+    public static void maxOfThreeInteger(Integer x, Integer y, Integer z) {
+/*
+    CompareTo method to test the maximum number
+ */
+        Integer maximum = x;
+        if (y.compareTo(maximum) > 0)
+            maximum = y;
+        if (z .compareTo(maximum) > 0)
+            maximum = z;
+        System.out.println(maximum+ " is the maximum of Three Integers");
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to Generic");
+
+        System.out.println("Maximum Of 3 Variables");
+        //TC1.1: Given MaximumNumber at first position
+        maxOfThreeInteger(60,30, 50);
+        //TC1.2: Given MaximumNumber at second position
+        maxOfThreeInteger(60,80, 50);
+        //TC1.3: Given MaximumNumber at third position
+        maxOfThreeInteger(60,30, 70);
     }
 }
